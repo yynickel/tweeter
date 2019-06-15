cda33b6a07ebd15291df446d2bf9d40b40bfb6d9
Nick Yu <yynickel@gmail.com>
2019-06-15 01:34:37 +0000

    refactored env variables to deploy on heroku

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@
 const PORT = 8080;
 const express = require("express");
 const bodyParser = require("body-parser");
+require('dotenv').config();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
